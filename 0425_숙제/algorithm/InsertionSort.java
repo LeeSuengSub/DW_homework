2376f59e51904abac610b8ab7f5f33bd5024f83e
LeeSuengSub <zlzm2522@gmail.com>
2022-04-28 13:52:42 +0900

    feat: 삽입정렬

--- 0425_숙제/algorithm/InsertionSort.java
+++ 0425_숙제/algorithm/InsertionSort.java
@@ -3,8 +3,26 @@ package 삽입정렬;
 public class InsertionSort {
 	
 	public static void main(String[] args) {
-        //삽입정렬을 이용하여 오름차순 정렬!
-		int array[] = {8, 5, 6, 2, 4};
-		        		
+
+		int arr [] = {8,5,6,2,4};
+		if(arr.length > 1) {
+			int i = 0; 
+			int j =0;
+			for(i=1; i<arr.length; ++i) {
+				int tmp = arr[i];
+				for(j=i-1; j>=0; --j) {
+					arr[j+1] = arr[j];
+					if(tmp > arr[j]) {
+						break;
+					}
+				}
+				arr[j+1] = tmp;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
 	}
+
 }
+	}
